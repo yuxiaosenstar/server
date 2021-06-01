7cfbebe5f8a4c929a0dc4eacdb014c81812993f1
yuchangchang <yuchangchang@hd123.com>
2021-06-01 10:44:41 +0800

    - 1

--- src/index.js
+++ src/index.js
@@ -185,6 +185,20 @@ app.use(
       // 保留文件扩展名
       keepExtensions: true,
       maxFileSize: 200 * 1024 * 1024, // 设置上传文件大小最大限制，默认2M
+      onFileBegin:(name,file) => {
+        // console.log(file);
+        // 获取文件后缀
+        const ext =getUploadFileExt(file.name);
+        // 最终要保存到的文件夹目录
+        const dir = path.join(__dirname,`public/upload`);
+        // 检查文件夹是否存在如果不存在则新建文件夹
+        checkDirExist(dir);
+        // 重新覆盖 file.path 属性
+        file.path = `${dir}/${getUploadFileName(ext)}`;
+      },
+      onError:(err)=>{
+        console.log(err);
+      }
     },
   })
 )
